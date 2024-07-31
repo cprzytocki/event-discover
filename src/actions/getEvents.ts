cd673ccf24b725c95ba0bb865432cd7b12245928
Chris Przytocki <cprzytocki@gmail.com>
2024-07-31 13:47:09 -0400

    add start and end date to query, fix image host

--- src/actions/getEvents.ts
+++ src/actions/getEvents.ts
@@ -3,10 +3,17 @@ import { EventsResponse } from "@/types/ticketMaster";
 
 const API_KEY = process.env.TICKETMASTER_API_KEY;
 
-export async function getEvents() {
-  const res = await fetch(
-    `https://app.ticketmaster.com/discovery/v2/events.json?countryCode=US&apikey=${API_KEY}`,
-  );
+export async function getEvents(startDate: string, endDate: string, location: string) {
+  const startQuery = startDate
+    ? `&startDateTime=${startDate}T00:00:00Z`
+    : `&startDateTime=${new Date().toISOString().split("T")[0]}T00:00:00Z`;
+
+  const endQuery = endDate ? `&endDateTime=${endDate}T23:59:59Z` : "";
+
+  console.log({ startQuery, endQuery, location });
+  const endpoint = `https://app.ticketmaster.com/discovery/v2/events.json?countryCode=US&sort=date,asc&apikey=${API_KEY}`;
+  const res = await fetch(endpoint + startQuery + endQuery);
+
   const data: Promise<EventsResponse> = res.json();
   return data;
 }
