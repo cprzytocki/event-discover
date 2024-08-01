47cb66de1854d5b7a845d8bed5d47c6d959b3a59
Chris Przytocki <cprzytocki@gmail.com>
2024-08-01 10:47:54 -0400

    change event request to include all countries

--- src/actions/getEvents.ts
+++ src/actions/getEvents.ts
@@ -8,7 +8,7 @@ export async function getEvents(startDate: string, endDate: string, city: string
   const endQuery = endDate ? `&endDateTime=${endDate}` : "";
   const cityQuery = city ? `&city=${city}` : "";
 
-  const endpoint = `https://app.ticketmaster.com/discovery/v2/events?countryCode=US&results=100&apikey=${API_KEY}`;
+  const endpoint = `https://app.ticketmaster.com/discovery/v2/events?&results=100&apikey=${API_KEY}`;
   const res = await fetch(endpoint + startQuery + endQuery + cityQuery);
 
   if (!res.ok) {
